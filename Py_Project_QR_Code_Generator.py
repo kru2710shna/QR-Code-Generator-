1678f3aff5eb2ff8177a1a7baabf2f842f3eb116
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2023-10-02 01:48:56 -0700

    Update Py_Project_QR_Code_Generator.py

--- Py_Project_QR_Code_Generator.py
+++ Py_Project_QR_Code_Generator.py
@@ -3,24 +3,32 @@
 # save a code as image 
 # run the function 
 
-
 import qrcode
 
-def generate_qrcode(text):
-    qr = qrcode.QRCode(
-        version = 1,
-        error_correction = qrcode.constants. ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
+# Define the data you want to encode in the QR code
+data = "Hello, QR Code!"
 
-    qr.add_data(text)
-    qr.make(fit=True)
-    img = qr.make_image(fill_color="black", back_color="white")
-    img.save("qrimg001.png")
-    
-url = input("Enter your url: ")
-generate_qrcode(url)
+# Create a QR code object
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+# Add data to the QR code
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR code
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the QR code image to a file
+img.save("my_qr_code.png")
+
+# Display the QR code
+img.show()
+
 
    
     
